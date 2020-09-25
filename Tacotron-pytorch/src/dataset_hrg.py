06f924dfefca7dc3134f333c1132fede93712e14
Aman Madaan <amadaan@cs.cmu.edu>
2020-09-25 12:02:11 -0400

    Refactor dataset HRG to enable audio generation from HRG

--- Tacotron-pytorch/src/dataset_hrg.py
+++ Tacotron-pytorch/src/dataset_hrg.py
@@ -6,6 +6,7 @@ import torch
 from collections import Counter
 from functools import partial
 from torch_geometric.data import Data, Dataset
+import torch_geometric
 from .symbols import txt2seq
 from .utils import read_json_from_pth
 
@@ -36,6 +37,101 @@ def _pad_2d(x, max_len):
                mode="constant", constant_values=0)
     return x
 
+class HRGVocab:
+    def __init__(self, id2tok, tok2id):
+        self.id2tok = id2tok
+        self.tok2id = tok2id
+        self.n_vocab = len(self.id2tok)
+        assert len(id2tok) == len(tok2id), "Invalid vocab"
+    
+    def get_tok2id(self, tok):
+        if tok in self.tok2id:
+            return self.tok2id[tok]
+        return self.tok2id["<UNK>"]
+    
+    @staticmethod
+    def from_dir(pth):
+        """Reads and initializes vocab from a directory
+
+        Args:
+            pth ([type]): [path that has id2tok and tok2id jsons.]
+
+        Returns:
+            [type]: [HRGVocab]
+        """
+        with open(f"{pth}/id2tok.json", "r") as f:
+            id2tok = json.load(f)
+        with open(f"{pth}/tok2id.json", "r") as f:
+            tok2id = json.load(f)
+        return HRGVocab(id2tok=id2tok, tok2id=tok2id)
+
+    def __len__(self):
+        return len(self.tok2id)
+
+class HRG:
+    """Place holder class for HRGs.
+    Currently just a wrapper around json
+    """
+
+    def __init__(self, hrg_json, vocab: HRGVocab):
+        """
+
+        Args:
+            hrg_json ([dict]): [HRG in json format]
+            vocab ([HRGVocab]): [a vocab object that can be used to map the symbols in this HRG]
+        """
+        self.hrg_json = hrg_json
+        self.vocab = vocab
+    
+    def to_pytorch_geom_graph(self) -> torch_geometric.data.Data:
+        """
+        Converts the HRG to graph,
+
+        NOTE: idx -> index, a way to identify each node in the graph
+            ids -> id for a token returned by the vocab.
+            Idxs are primarily used for specifying the connectivity of the graph
+        Returns:
+            Edge index: (num_edges, 2)
+            Node features: (num_nodes, feature_dim)
+        """
+        words, sylls = [], []
+        node_idx = {}
+        node_ids = []
+        x = []
+
+        edges = []
+
+        syll_node_idxs = []
+        for i, word_rep in enumerate(self.hrg_json):
+            word_node = f"{word_rep['word']}-{i}"
+            word_node_id = self.vocab.get_tok2id(word_rep['word'])
+            node_idx[word_node] = len(node_idx)
+            x.append(word_node_id)
+
+            for j, daughter in enumerate(word_rep["daughters"]):
+                # make syll node
+                syll_parent_node = ""
+                for syll in daughter:
+                    syll_parent_node += syll["syll"]
+                syll_parent_node_id = self.vocab.get_tok2id(syll_parent_node)
+                x.append(syll_parent_node_id)
+                syll_parent_node = f"{syll_parent_node}-{i}-{j}"
+                node_idx[syll_parent_node] = len(node_idx)
+                edges.append([node_idx[word_node], node_idx[syll_parent_node]])
+                # now prepare phone nodes
+                for k, syll in enumerate(daughter):
+                    
+                    syll_node = f"{syll['syll']}-{i}-{j}-{k}"
+                    syll_node_id = self.vocab.get_tok2id(syll['syll'])
+                    node_idx[syll_node] = len(node_idx)
+                    x.append(syll_node_id)
+                    syll_node_idxs.append(node_idx[syll_node])
+
+                    edges.append([node_idx[syll_parent_node], node_idx[syll_node]])
+
+        return Data(x=torch.tensor(x, dtype=torch.long), edge_index=torch.tensor(edges, dtype=torch.long).contiguous().t(),
+                    syll_nodes=torch.tensor(syll_node_idxs, dtype=torch.long))
+
 
 class MyDataset(Dataset):
     """Graph Dataset
@@ -56,81 +152,20 @@ class MyDataset(Dataset):
                 meta['hrg'].append(hrg)
                 meta['mel'].append(fmel)
                 meta['spec'].append(fspec)
-        with open(f"{data_dir}/id2tok.json", "r") as f:
-            self.id2tok = json.load(f)
-        with open(f"{data_dir}/tok2id.json", "r") as f:
-            self.tok2id = json.load(f)
-            self.n_vocab = len(self.tok2id)
         
-        self.hrgs = [json.loads(hrg) for hrg in meta['hrg']]
-        self.init_X()
+        # make vocab
+        self.vocab = HRGVocab.from_dir(data_dir)
+        self.n_vocab = len(self.vocab)
+
+        # Read HRGs, convert each HRG to a Pytorch Geom object
+        self.hrgs = [HRG(hrg_json=json.loads(hrg), vocab=self.vocab) for hrg in meta['hrg']]
+        self.X = [hrg.to_pytorch_geom_graph() for hrg in self.hrgs]
+
+        # Read audios
         self.Y_mel = [os.path.join(data_dir, f) for f in meta['mel']]
         self.Y_spec = [os.path.join(data_dir, f) for f in meta['spec']]
         assert len(self.X) == len(self.Y_mel) == len(self.Y_spec)
 
-    def init_X(self):
-        """Parse the X by doing the following:
-            - Use all the HRGs to create a vocab (for vanilla Tacotron, the symbol set is fixed 
-            so everything can be done in symbols.py)
-            - Set X[i] = hrg_to_graph(hrg[i])
-        """
-        self.X = [self.hrg_to_graph(hrg) for hrg in self.hrgs]
-
-    def get_tok2id(self, tok):
-        if tok in self.tok2id:
-            return self.tok2id[tok]
-        return self.tok2id["<UNK>"]
-
-    def hrg_to_graph(self, hrg):
-        """
-        Converts the HRG to graph,
-
-        NOTE: idx -> index, a way to identify each node in the graph
-            ids -> id for a token returned by the vocab.
-            Idxs are primarily used for specifying the connectivity of the graph
-        Returns:
-            Edge index: (num_edges, 2)
-            Node features: (num_nodes, feature_dim)
-        """
-        words, sylls = [], []
-        node_idx = {}
-        node_ids = []
-        x = []
-
-        edges = []
-
-        syll_node_idxs = []
-        for i, word_rep in enumerate(hrg):
-            word_node = f"{word_rep['word']}-{i}"
-            word_node_id = self.get_tok2id(word_rep['word'])
-            node_idx[word_node] = len(node_idx)
-            x.append(word_node_id)
-
-            for j, daughter in enumerate(word_rep["daughters"]):
-                # make syll node
-                syll_parent_node = ""
-                for syll in daughter:
-                    syll_parent_node += syll["syll"]
-                syll_parent_node_id = self.get_tok2id(syll_parent_node)
-                x.append(syll_parent_node_id)
-                syll_parent_node = f"{syll_parent_node}-{i}-{j}"
-                node_idx[syll_parent_node] = len(node_idx)
-                edges.append([node_idx[word_node], node_idx[syll_parent_node]])
-                # now prepare phone nodes
-                for k, syll in enumerate(daughter):
-                    
-                    syll_node = f"{syll['syll']}-{i}-{j}-{k}"
-                    syll_node_id = self.get_tok2id(syll['syll'])
-                    node_idx[syll_node] = len(node_idx)
-
-                    x.append(syll_node_id)
-                    syll_node_idxs.append(node_idx[syll_node])
-
-                    edges.append([node_idx[syll_parent_node], node_idx[syll_node]])
-
-        return Data(x=torch.tensor(x, dtype=torch.long), edge_index=torch.tensor(edges, dtype=torch.long).contiguous().t(),
-                    syll_nodes=torch.tensor(syll_node_idxs, dtype=torch.long))
-                
     def __getitem__(self, idx):
         item = (self.X[idx],
                 np.load(self.Y_mel[idx]),
