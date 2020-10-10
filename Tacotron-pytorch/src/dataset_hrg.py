9447f961fbe922d292f0c7357b448b72d80d2a02
TanmayParekh <tparekh97@gmail.com>
2020-10-10 15:57:50 -0400

    Accommodating new dataset for HRG

--- Tacotron-pytorch/src/dataset_hrg.py
+++ Tacotron-pytorch/src/dataset_hrg.py
@@ -17,7 +17,15 @@ def getDataLoader(mode, meta_path, data_dir, batch_size, r, n_jobs, use_gpu, **k
         shuffle = False
     else:
         raise NotImplementedError
-    DS = MyDataset(meta_path, data_dir)
+
+    if "add_info_headers" in kwargs:
+        add_info_headers = kwargs["add_info_headers"]
+    
+    if len(add_info_headers):
+        DS = MyDatasetAddInfo(meta_path, data_dir)
+    else:
+        DS = MyDataset(meta_path, data_dir)
+        
     DL = torch.utils.data.DataLoader(
         DS, batch_size=batch_size, shuffle=shuffle, drop_last=False,
         num_workers=n_jobs, collate_fn=partial(collate_fn, r=r), pin_memory=use_gpu)
@@ -184,6 +192,63 @@ class MyDataset(Dataset):
     def __len__(self):
         return len(self.X)
 
+class MyDatasetAddInfo(Dataset):
+    """Dataset
+    """
+    def __init__(self, meta_path, data_dir):
+        # Load meta
+        # ---------
+        # text: texts
+        # mel : filenames of mel-spectrogram
+        # spec: filenames of (linear) spectrogram
+        #
+        meta = {'hrg':[], 'mel': [], 'spec': [], 'add_info': []}
+        with open(meta_path) as f:
+            for line in f.readlines():
+                # If there is '\n' in text, it will be discarded when calling symbols.txt2seq
+                # Read the file and integrate any additional info with the text itself
+                fmel, fspec, n_frames, hrg, add_info = line.split('|')
+                meta['hrg'].append(hrg)
+                meta['mel'].append(fmel)
+                meta['spec'].append(fspec)
+                meta['add_info'].append(add_info)
+
+        # Separate text and additional info
+        self.add_info = [ json.loads(t) for t in meta['add_info'] ]
+        headers = list(self.add_info[0].keys())
+            
+        # make vocab for each additional info
+        self.add_info_vocab = {}
+        for h in headers:
+            self.add_info_vocab[h] = Vocab.from_dir(data_dir, h)
+        # Convert to ids
+        self.add_info = [ {h:self.add_info_vocab[h].get_tok2id(t[h]) for h in t} for t in self.add_info ]
+        # get max vocab size for all the additional info
+        self.n_add_info_vocab = max([self.add_info_vocab[h].n_vocab for h in headers])
+
+        # make vocab for HRG
+        self.vocab = HRGVocab.from_dir(data_dir)
+        self.n_vocab = len(self.vocab)
+
+        # Read HRGs, convert each HRG to a Pytorch Geom object
+        self.hrgs = [HRG(hrg_json=json.loads(hrg), vocab=self.vocab)
+                     for hrg in meta['hrg']]
+        self.X = [hrg.to_pytorch_geom_graph() for hrg in self.hrgs]
+
+        self.Y_mel = [os.path.join(data_dir, f) for f in meta['mel']]
+        self.Y_spec = [os.path.join(data_dir, f) for f in meta['spec']]
+        assert len(self.X) == len(self.Y_mel) == len(self.Y_spec) == len(self.add_info)
+        
+    def __getitem__(self, idx):
+        item = (self.X[idx],
+                np.load(self.Y_mel[idx]),
+                np.load(self.Y_spec[idx]),
+                self.add_info[idx])
+        return item
+
+    def __len__(self):
+        return len(self.X)
+
 
 def collate_fn(batch, r):
     """
@@ -193,6 +258,7 @@ def collate_fn(batch, r):
     mel_batch: torch.FloatTensor (bsz * max_tgt_len)
     spec_batch: torch.FloatTensor (bsz * max_tgt_len)
     """
+    num_inputs = len(batch[0])
 
     x_batch = [x[0] for x in batch]
 
@@ -213,4 +279,8 @@ def collate_fn(batch, r):
                  dtype=np.float32)
     spec_batch = torch.FloatTensor(c)
 
-    return x_batch, n_phone_nodes, mel_batch, spec_batch
+    if num_inputs > 3:
+        add_info = [x[-1] for x in batch]
+        return x_batch, input_lengths, mel_batch, spec_batch, add_info
+    else:
+        return x_batch, input_lengths, mel_batch, spec_batch, None
